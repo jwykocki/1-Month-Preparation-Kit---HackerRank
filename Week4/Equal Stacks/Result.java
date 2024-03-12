9072665b5d4ab8df831020c345ab170cd9edc685
Kuba <jakub.wykocki@wp.pl>
2024-03-12 19:14:27 +0100

    Equal Stacks and Maximum Subarray

---
+++ Week4/Equal Stacks/Result.java
@@ -0,0 +1,90 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'equalStacks' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h1
+     *  2. INTEGER_ARRAY h2
+     *  3. INTEGER_ARRAY h3
+     */
+
+    public static int equalStacks(List<Integer> h1, List<Integer> h2, List<Integer> h3) {
+        
+        int sum1 = h1.stream().mapToInt(Integer::intValue).sum();
+        int sum2 = h2.stream().mapToInt(Integer::intValue).sum();
+        int sum3 = h3.stream().mapToInt(Integer::intValue).sum();
+        
+        while(true){
+            
+            
+            if(sum1 == sum2 && sum2 == sum3){
+                return sum1;
+            }
+            
+            int minHeight = Math.min(Math.min(sum1, sum2), sum3);
+            
+            if(sum1>minHeight){
+                sum1 -= h1.remove(0);
+            }
+            
+            if(sum2>minHeight){
+                sum2 -= h2.remove(0);
+            }
+            
+            if(sum3>minHeight){
+                sum3 -= h3.remove(0);
+            }
+        }
+        
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n1 = Integer.parseInt(firstMultipleInput[0]);
+
+        int n2 = Integer.parseInt(firstMultipleInput[1]);
+
+        int n3 = Integer.parseInt(firstMultipleInput[2]);
+
+        List<Integer> h1 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> h2 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> h3 = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.equalStacks(h1, h2, h3);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
---
+++ Week4/The Maximum Subarray/Result.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'maxSubarray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static List<Integer> maxSubarray(List<Integer> arr) {
+    // Write your code here
+    int maxSubarraySum = Integer.MIN_VALUE;
+        int maxSubsequenceSum = 0;
+        int currentSum = 0;
+        int maxElement = Integer.MIN_VALUE;
+        
+        for (int num : arr) {
+            currentSum = Math.max(num, currentSum + num);
+            maxSubarraySum = Math.max(maxSubarraySum, currentSum);
+            
+            if (num > 0) {
+                maxSubsequenceSum += num;
+            }
+            
+            if (num > maxElement) {
+                maxElement = num;
+            }
+        }
+        
+        if (maxSubsequenceSum == 0) {
+            maxSubsequenceSum = maxElement;
+        }
+        
+        return Arrays.asList(maxSubarraySum, maxSubsequenceSum);
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                List<Integer> result = Result.maxSubarray(arr);
+
+                bufferedWriter.write(
+                    result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                    + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
